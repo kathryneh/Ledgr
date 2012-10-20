07170c743df559b0385eead99ebd9f8706b9dc24
Katie Hawthorne <kathryne.h@gmail.com>
2012-10-19 18:18:32 -0700

    Adding the list of todo items for the programmatic shell ui.

---
+++ ledgr.js
@@ -0,0 +1,11 @@
+/*Ledgr.js
+ *
+ *This will (hopefully) programmatically build out the UI. 
+ *
+ *Things to build: New Rows
+ * 				   Form
+ * 				   Alerts
+ *				   
+ */
+
+// The shell probably doesn't need to be programmatically created, as it doesn't really make sense
