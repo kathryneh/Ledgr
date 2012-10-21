8375f6cb3207ba73306891ef57ac87e4d0a22b83
Katie Hawthorne <kathryne.h@gmail.com>
2012-10-20 18:41:01 -0700

    example of using programmatic building of a UI for Mike

--- ledgr.js
+++ ledgr.js
@@ -8,4 +8,33 @@
  *				   
  */
 
-// The shell probably doesn't need to be programmatically created, as it doesn't really make sense
+
+	$(document).ready(ready);
+
+	function ready(){
+		var pageTitle = "Ledgr";
+		var userTitle = "alkali";
+		var imgSrc = "http://www.placekitten.com/40/40";
+		var userImgSrc = "http://www.placekitten.com/40/40";
+		initHeader(pageTitle, userTitle, imgSrc, userImgSrc);
+	}
+
+	function initHeader(pageTitle, userTitle, imgSrc, userImgSrc){
+		var container = $("<div class='container'></table>");
+		var header = "<div class='header'><div class='logo'><img src='" ;
+		header+= imgSrc;
+		header+= "' alt='ninja'><span id='pagetitle'>"; 
+		header+= pageTitle;
+		header+= "</span></div></div>";
+		header = $(header);
+		var userLogo = "<img src="+userImgSrc+" alt='"+userTitle+"'><span id='username'>"+userTitle+"</span>";
+		header.append(userLogo);
+		container.append(header);
+		$('body').append(container);
+		initNav();
+	}
+	function initNav(){
+		var nav = $('<nav></nav>');
+		var ulMain = $('<ul class="main-nav"><ul>');
+		var liSelected = $('<li id="selected"> home </li>');
+	}
